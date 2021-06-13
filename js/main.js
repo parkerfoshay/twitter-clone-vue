da0c9871593a9c37acfe3e7f2513b5218442c75c
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-06-13 16:29:25 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+let dataObject = {
+  userData: {},
+  userID: 0,
+  name: "",
+  email: "",
+  password: "",
+  max_length: 30,
+  max_pass_length: 16,
+  error: "",
+  registered: false,
+  tweetMsg: "",
+  tweets: [],
+  max_tweet: 200,
+};
+
+let methodsObject = {
+  registerAccount() {
+    if (this.name !== "" && this.email !== "" && this.password !== "") {
+      this.userData.userID = this.userID;
+      this.userData.name = this.name;
+      this.userData.email = this.email;
+      this.userData.password = this.password;
+      this.registered = true;
+    } else {
+      this.error = "Incomplete form!";
+    }
+
+    /* Add registration data to the local storage */
+    localStorage.setItem("simple_tweet_registered", true);
+    /* Add the whole userData object as JSON string */
+    localStorage.setItem(
+      "simple_tweet_registered_user",
+      JSON.stringify(this.userData)
+    );
+
+    this.name = "";
+    this.email = "";
+    this.password = "";
+  },
+
+  created() {
+    if (localStorage.getItem("simple_tweet_registed") === "true") {
+      this.registered = true;
+    }
+
+    if (localStorage.getItem("simple_tweet_registered_user")) {
+      this.userData = JSON.parse(
+        localStorage.getItem("simple_tweet_registered_user")
+      );
+    }
+
+    if (localStorage.getItem("simple_tweet_tweets")) {
+      console.log("There is a list of tweets");
+      this.tweets = JSON.parse(localStorage.getItem("simple_tweet_tweets"));
+    } else {
+      console.log("No tweets here");
+    }
+  },
+
+  sendTweet() {
+    this.tweets.unshift({
+      text: this.tweetMsg,
+      date: new Date().toLocaleTimeString(),
+    });
+
+    this.tweetMsg = "";
+    console.log(this.tweets);
+
+    stringTweets = JSON.stringify(this.tweets);
+
+    localStorage.setItem("simple_tweet_tweets", stringTweets);
+  },
+};
+
+var app = new Vue({
+  el: "#app",
+  data: dataObject,
+  methods: methodsObject,
+});
